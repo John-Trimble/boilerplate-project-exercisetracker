2e87a8eac5046a186e62ec457625fa64f575275f
John-Trimble <jhtrimble@me.com>
2021-01-10 22:14:26 -0800

    Update log

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const shortId = require('shortid')
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://John:3216@fcc-cluster.edllc.mongodb.net/Excercise-Tracker?retryWrites=true&w=majority', { useNewUrlParser: true, useFindAndModify: true, useUnifiedTopology: true })
@@ -18,7 +17,6 @@ const ExerciseSchema = new mongoose.Schema({
 
 const UserSchema = new mongoose.Schema({
   username: {type: String, required: true},
-  id: {type: String, required: true, unique: true},
   exercises: [ExerciseSchema]
 })
 const Users = mongoose.model("Users", UserSchema);
@@ -39,10 +37,11 @@ app.post("/api/exercise/new-user", (req, res) => {
     console.log(err, data)
     if (err) return res.send({error: "Failed to fetch data"})
     if (data.length != 0) return res.send({error: "Username taken"})
-    let user = new Users({username: req.body.username, id: shortId.generate()})
+    let user = new Users({username: req.body.username})
     user.save((e, d) => {
       if (e) return console.error.bind(console, "Failed to add new user")
-      res.send({username: d.username, _id: d.id})
+      let {username, id} = d;
+      res.send({username, id})
     })
   })
 })
@@ -50,20 +49,48 @@ app.post("/api/exercise/new-user", (req, res) => {
 app.get("/api/exercise/users", (req, res) => {
   Users.find({}, (err, data) => {
     if (err) return res.send({error: "Failed to fetch users"})
-    res.send(data.map(item => {return {username: item.username, _id: item.id}}))
+    res.send(data.map(item => {let {username, id} = item;return {username, id}}))
   })
 })
 
 app.post("/api/exercise/add", (req, res) => {
-  Users.findOne({id: req.body.userId}, (err, data) => {
+  Users.findById(req.body.userId, (err, data) => {
     if (err || data == null) return res.send({error: "Failed to find user"})
     let {duration, description} = req.body;
     let exDate = new Date(req.body.date == ""?Date.now():req.body.date)
-    Users.findOneAndUpdate({id: req.body.userId}, {$push: {exercises: {duration, description, date: exDate}}}, {new: true}, (e, d) => {
+    Users.findOneAndUpdate({_id: req.body.userId}, {$push: {exercises: {duration, description, date: exDate}}}, {new: true}, (e, d) => {
       if (e) return res.send({error: "Failed to update user"})
-      let {id, username, exercises} = d
-      res.send({id, username, exercises})
+      let {_id, username, exercises} = d
+      res.send({_id, username, exercises})
     })
+  })
+})
+
+app.get("/api/exercise/log", (req, res) => { //userId, from, to, limit
+  const query = req.query
+  if (!query.userId) return res.send({error: "No userId field"})
+  Users.findById(query.userId, (err, data) => {
+
+    if (err) return res.send({error: "Failed to Fetch Users"})
+    let dateRegex = /\d{4}-\d{2}-\d{2}/
+    let fromMatch = (!query.from || query.from.match(dateRegex)), toMatch = (!query.to || query.to.match(dateRegex));
+    if (!fromMatch || !toMatch) return res.send({error: "Parameters Invalid"})
+    let {_id, username, exercises} = data
+    console.log(new Date(query.to), new Date(query.from))
+
+    exercises = exercises.filter((item, index) => {
+      if (query.from.match(dateRegex)) { //from filter
+        console.log(new Date(fromMatch[0]).getTime(), new Date(item.date).getTime())
+        if (new Date(fromMatch[0]).getTime() >= new Date(item.date).getTime()) {return false}
+      }
+      if (query.to.match(dateRegex)) { //to filter
+        console.log(new Date(toMatch[0]).getTime(), new Date(item.date).getTime())
+        if (new Date(toMatch[0]).getTime() <= new Date(item.date).getTime()) {return false}
+      }
+      return (index < query.limit || !query.limit) //limit filter
+    })
+
+    res.send({_id, username, exercises})
   })
 })
 
